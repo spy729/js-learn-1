9d8b2f490677d72aa693be737f35945ecd694a7a
Rishi Agarwal <116219874+spy729@users.noreply.github.com>
2024-06-07 19:22:08 +0000

    stack and heap logic

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,23 @@
+// primitive data types => number ,  string ,  boolean , null , undifined , symbol , bigInt 
+
+// Refrence (non - premitive data type )
+// array , object , functions 
+
+//  stack is primitive and heap is non primitive 
+// in stack when the data is changed it is done in its copy but in heap it is done by refrence 
+
+let name1 = "rishi agarwal"
+let name2 = name1;
+name2 = "kc"
+console.log(name1);
+console.log(name2)
+
+let student1 = {
+    name: "rishi",
+    roll_no : 144
+}
+
+let student2 = student1;
+student2.name = "kk"
+console.log(student1.name);
+console.log(student2.name)
