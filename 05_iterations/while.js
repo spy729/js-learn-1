23a139f3bbb6ae18f0ec6eff506e4efc0c66f068
Rishi Agarwal <116219874+spy729@users.noreply.github.com>
2024-06-20 16:54:54 +0000

    while loop done

---
+++ 05_iterations/while.js
@@ -0,0 +1,22 @@
+// while(condition){
+//     // function to be executed 
+// }
+
+let index = 10
+while(index>0){
+    console.log(index)
+    index--;
+}
+
+let Array1 = ["superman" , "batman" , "flash"]
+let arr = 0
+while(arr < Array1.length){
+    console.log(`value of index is ${Array1[arr]}`)
+    arr++
+}
+
+let score = 5
+do{
+    console.log(`score is ${score}`)
+    score--
+}while(score>0)
